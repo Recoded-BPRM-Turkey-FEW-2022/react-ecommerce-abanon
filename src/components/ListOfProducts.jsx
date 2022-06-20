74305af9de4f9150579538ca814cc0f2b6a010a4
Amjad <amjadmaqsuma@gmail.com>
2022-06-20 12:21:05 +0300

    some edit

--- src/components/ListOfProducts.jsx
+++ src/components/ListOfProducts.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Card from "./Card"
-// const ListOfProducts = ({ info }) => {
-//     return (
-//         <div>{/*Interate over products and create one card */}
-//         {info.map((product)=>{
+const ListOfProducts = ({ info }) => {
+    return (
+        <div>{/*Interate over products and create one card */}
+     {/* {info.map((product)=>{
 
-//         <Card image = {product.image} title ={product.title}  key={product.id}/>
-//         })}
-//             </div>)
-// }
-// export default ListOfProducts;
+        <Card image = {product.image} title ={product.title}  key={product.id}/>
+        })} */}
+            </div>)
+}
+export default ListOfProducts;
