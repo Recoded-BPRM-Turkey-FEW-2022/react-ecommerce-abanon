3c063b3cee746019e5650d891446c3115cd3fba9
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-23 13:57:54 +0300

    search bar almost ready but need to show the cards

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import {useEffect} from "react";
 import {AppBar,Toolbar,Typography,}from "@mui/material"
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
-import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Categories from "./Categories";
+import Search from "./Search";
+
+
 // // For Ghofran....
 //to include "Search", "Category", "Cart", "Home (AllProducts)"
 const NavBar = () => {
+    const [Info, setInfo] = useState([])
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(data => setInfo(data))
+        }, [])
+
     return (
     <AppBar position='static' style={{ background: '#2E3B55' }}>
     <Toolbar >
@@ -19,18 +26,7 @@ const NavBar = () => {
     </Typography>
     <Button href="/allProducts" sx={{ mr:6 , color:"#ffffff"}}>Products</Button>
     <Categories/>
-    <Paper
-    component="form"
-    sx={{ p: '1px 2px', display: 'flex', alignItems: 'end', width: 250 }}>
-    <InputBase
-    sx={{ ml: 1, flex: 1 }}
-    placeholder="Search"
-    inputProps={{ 'aria-label': 'search' }}/>
-    <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-    <IconButton type="submit" sx={{ p: '10px' }} aria-label="Search">
-    <SearchIcon />
-    </IconButton>
-    </Paper>
+    <Search Info= {Info} setInfo={setInfo} />
     <ShoppingCartIcon sx={{fontSize:'30' , cursor: "pointer" , ml:3}}/>
     </Toolbar>
     </AppBar>
