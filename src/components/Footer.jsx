a6b15ba52b63b77d09c6261c61258fb981fe8b7f
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-24 15:00:38 +0300

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,34 @@
 //Copyrights and people who contributed to this file and their linkedIn, GIthub...etc
 import React from "react";
+import { BottomNavigation ,Toolbar, Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
 
 const Footer = () => {
     return (
-        <div>FOOTER IS HERE</div>
+        <>
+        <BottomNavigation    style={{ background: '#2E3B55' ,position='absolute' , bottom :'0' }}>
+        <Toolbar sx={{justifyContents:"space-between"}} >
+        
+        <Typography variant="h4" sx={{flexGrow: "1",cursor: "pointer"}} >
+        AMJAD
+    </Typography>
+    <GitHubIcon/>
+        <Divider style={{ borderColor: "#ffffff"}} sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        
+        <div>
+        GHUFRAN
+        </div>
+        <Divider style={{ borderColor: "#ffffff"}}  sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <div>
+        ABDU
+        </div>
+        </Toolbar>
+        </BottomNavigation>
+        </>
     )
 }
 export default Footer;
