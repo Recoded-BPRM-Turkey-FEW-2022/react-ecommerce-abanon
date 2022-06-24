c12a75884c6108885521506f1573b890aebdb7b9
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-24 12:09:13 +0300

    display cart is ready and footer need some work

--- src/components/DisplayCart.jsx
+++ src/components/DisplayCart.jsx
@@ -1,8 +1,16 @@
 // I think here we will display cart items and then we will display the total price.
-import react from 'react';
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { Button} from '@mui/material';
 
 
-const DisplayCart = ({data})=>{
+const DisplayCart = ({data,mutation})=>{
 
     
 // console.log(data)
@@ -10,15 +18,65 @@ const DisplayCart = ({data})=>{
 return (
 
 <>
-<div>
-<h1>
-{data[0].title}
-</h1>
-<button onClick={()=>{console.log(data)}}>
-    
+<button onClick={()=>{console.log(data)
+    ; mutation(data[0])}}>
 Clik me 
 </button>
-</div>
+
+{data.map((Item)=>{
+    return (
+    <>
+    
+    <List  sx={{ ml:14 , width: '100%', maxWidth: '70%' , bgcolor: 'background.paper' , }}>
+      <ListItem sx={{maxWidth:'100%'}} alignItems="center">
+        <ListItemAvatar >
+          <Avatar style = {{width:'100px', height: '100px'}} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText sx={{ml:10 , mt:1}}
+          primary= "title"
+          secondary={
+            <React.Fragment >
+              <Typography
+                sx={{ display: 'inline'}}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Quantity: 10
+              </Typography>
+
+
+              <Typography
+                sx={{ display: 'inline' ,ml:2 }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+                
+              >
+                
+               price {data.price} 
+              </Typography>
+              
+            </React.Fragment>
+            
+          }
+        />
+        <Button  size="small" color="primary">
+              Remove 
+        </Button>
+        
+              
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      
+    </List>
+    
+    
+    </>
+    )
+})}
+
+   
 
 </>
 
