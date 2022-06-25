75876212f2a17bbf876d0dc082c727c673ab7ca8
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-25 09:52:45 +0300

    Website is ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const Home = () => {
 
-    return(<div className="Home" style={{margin:"10px", padding:"5px"}}><h3 style={{textAlign:"center" }}>Welcome to our MiniShop</h3><div className="parag"><p>
-    This E-commerce site was made using a fake API from <a href="https://www.escuelajs.co">EscuelaJS</a>.{<br/>}
-    As a mock project to practice front-end development using React. 
+    return(<div className="textHome" style={{height: "100%" ,background:'#fafafa' , margin:"40", padding:"5" }} >
+        <h2 style={{textAlign:"center" }}>Welcome to our MiniShop</h2>
+        <div className="parag" style={{ marginTop:"40", padding:"5" }}>
+        <p>
+    *This E-commerce site was made using a fake API from <a href="https://www.escuelajs.co">EscuelaJS</a>.{<br/>}
+    *As a mock project to practice front-end development using React. 
     </p>
-    <p> You can explore the site by selecting a button from the navigation bar.{<br/>}
-    You can also check <a href="https://github.com/Recoded-BPRM-Turkey-FEW-2022/react-ecommerce-abanon">source code on GitHub </a> 
-     if you are interested in the technical stuff!.{<br/>}
-     External links to the creators of this project were added to the footer. Enjoy your time {":)"}
+    <p>*You can explore the site by selecting a button from the navigation bar.{<br/>}
+    *You can also check <a href="https://github.com/Recoded-BPRM-Turkey-FEW-2022/react-ecommerce-abanon">source code on GitHub </a> 
+     *If you are interested in the technical stuff!.{<br/>}
+     *External links to the creators of this project were added to the footer. Enjoy your time {":)"}
     </p>
     </div>
     </div>)
